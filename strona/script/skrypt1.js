6d75762a09bba691061bd5cbd2d3594fab23926d
trojanowskik <karoltro@wp.pl>
2021-12-16 20:01:09 +0100

    master

--- strona/script/skrypt1.js
+++ strona/script/skrypt1.js
@@ -5,4 +5,21 @@ let witacz = document.getElementById("witacz")
 let odwiedzajacy = prompt("Wpisz swoje imię");
   if (odwiedzajacy !== null) {
     witacz.innerText = "Witaj na stronie " + odwiedzajacy + "!";
-  }
+  };
+
+  require([
+  "esri/Map",
+    "esri/views/MapView"
+  ], (Map, MapView) => {
+
+    const map1 = new Map({
+      basemap: "streets-night-vector"
+    })
+
+    const view = new MapView({
+      map: map1,
+      container: "mapa",
+      center: [-100.2424, 36,12424],
+      zoom: 13
+    })
+  });
